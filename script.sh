7989b4fbd98c7124a133c82b8d03e2bbeb36f3b2
patrickluizdev <patrickluizjf@hotmail.com>
2023-01-07 23:52:50 -0300

    teste

--- script.sh
+++ script.sh
@@ -13,6 +13,7 @@ LOCAL_VERSION=$(git rev-parse HEAD)
 # Comparar as duas versões e exibir uma mensagem de acordo
 if [ "$LATEST_VERSION" == "$LOCAL_VERSION" ]; then
   echo "O repositório está atualizado."
+  
 else
   echo "O repositório precisa ser atualizado."
 
