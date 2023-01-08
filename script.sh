c33c23b133a3e222927fdd43b8f0308754ed17c5
patrickluizdev <patrickluizjf@hotmail.com>
2023-01-08 00:10:16 -0300

    ADD modo de uso

--- script.sh
+++ script.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
+# Coloque o script dentro do repositorio
+# Utilize "chmod +x script.sh" para da permissão para o funcionamento do script
+# Para deixar o script em segundo plano utilize o comando " nohup ./script.sh & "
+
+
+
+while true; do
 
 # Variáveis ​​do repositório e da chave de acesso à API do GitHub
 REPO_OWNER="patrickluizdev"
 REPO_NAME="gitVersion-Container"
 
+
 # Obter a última versão do repositório usando a API do GitHub
 LATEST_VERSION=$(curl -s https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/git/refs/heads/master)
+
 
 # Verificar a versão do repositório local
 LOCAL_VERSION=$(git rev-parse HEAD)
 
+
 # Comparar as duas versões e exibir uma mensagem de acordo
 if [ "$LATEST_VERSION" == "$LOCAL_VERSION" ]; then
+
   echo "O repositório está atualizado."
-  
+
 else
   echo "O repositório precisa ser atualizado."
 
@@ -34,3 +45,10 @@ else
 
   echo "O repositório foi atualizado."
 fi
+
+sleep 60
+done
+
+# Para adicionar chave em repositorio privado use:
+# GITHUB_API_KEY="sua chave de acesso aqui"
+# LATEST_VERSION=$(curl -s -H "Authorization: token $GITHUB_API_KEY" https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/git/refs/heads/master | jq .object.sha)
